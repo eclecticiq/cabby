c20b89e073e8d29f9307c9e93a311af918581c8a
Sergey Polzunov <sergey@polzunov.com>
2015-01-13 12:33:12 +0100

    new version and name tweak

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup, find_packages
 
 setup(
-    name = "taxii_client",
-    version = "0.0.1",
+    name = "taxii-client",
+    version = "0.0.2",
 
     packages = find_packages(),
 
