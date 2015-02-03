f804e403832a3cedfdb1465e3775cd99ffbca208
Wouter Bolsterlee <wouter@intelworks.com>
2015-02-03 13:48:58 +0100

    Add required PyPI metadata to setup.py
    See #5.

--- setup.py
+++ setup.py
@@ -3,6 +3,9 @@ from setuptools import setup, find_packages
 setup(
     name="taxii-client",
     version="0.0.2",
+    url="https://github.com/Intelworks/taxii-client/",
+    author="Intelworks",
+    author_email="founders@intelworks.com",
     packages=find_packages(),
     scripts=[
         'bin/taxii-discovery',
