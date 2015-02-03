a2a2bfd4f75513f808dfec962c2f1be55bfe08db
Wouter Bolsterlee <wouter@intelworks.com>
2015-02-03 13:48:09 +0100

    PEP8 setup.py

--- setup.py
+++ setup.py
@@ -1,20 +1,17 @@
 from setuptools import setup, find_packages
 
 setup(
-    name = "taxii-client",
-    version = "0.0.2",
-
-    packages = find_packages(),
-
-    scripts = [
+    name="taxii-client",
+    version="0.0.2",
+    packages=find_packages(),
+    scripts=[
         'bin/taxii-discovery',
         'bin/taxii-poll',
-        'bin/taxii-push'
+        'bin/taxii-push',
     ],
-
-    install_requires = [
+    install_requires=[
         'libtaxii==1.1.105-SNAPSHOT',
         'pytz',
-        'colorlog'
+        'colorlog',
     ]
 )
