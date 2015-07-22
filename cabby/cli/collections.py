6f7b7291ed77e142b6857f52b85e28e1bc9a14af
Sergey Polzunov <sergey@polzunov.com>
2015-07-22 10:43:58 +0200

    get_feeds is not there anymore

--- cabby/cli/collections.py
+++ cabby/cli/collections.py
@@ -2,24 +2,17 @@ from libtaxii.constants import SVC_COLLECTION_MANAGEMENT, SVC_FEED_MANAGEMENT
 
 from .commons import run_client, get_basic_arg_parser
 
-def _get_collections(client, path):
-
-    if hasattr(client, 'get_collections'):
-        return client.get_collections(uri=path)
-    elif hasattr(client, 'get_feeds'):
-        return client.get_feeds(uri=path)
-
-
 def _runner(client, path, args):
 
     collections = []
 
     if path:
-        collections.extend(_get_collections(client, path))
+        collections.extend(client.get_collections(uri=path))
     else:
-        for service in client.get_services(service_types=[SVC_COLLECTION_MANAGEMENT, SVC_FEED_MANAGEMENT]):
-            _collections = _get_collections(client, service.service_address)
-            collections.extend(_collections)
+        service_types = [SVC_COLLECTION_MANAGEMENT, SVC_FEED_MANAGEMENT]
+        for service in client.get_services(service_types=service_types):
+            collections.extend(
+                client.get_collections(uri=service.service_address))
 
     for c in collections:
         if args.as_xml:
